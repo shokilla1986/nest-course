b97c1d478f8772c2f72165f7acb9229d9a04918f
Dmitrii <shokilla@mail.ru>
2022-10-14 13:18:40 +0400

    created module calcelate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CalculateModule } from './calculate/calculate.module';
 
 @Module({
-  imports: [],
+  imports: [CalculateModule],
   controllers: [AppController],
   providers: [AppService],
 })
