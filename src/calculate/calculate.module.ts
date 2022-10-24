b1821ea2114c42ee58f8de164944daae8ebb9f92
Dmitrii <shokilla@mail.ru>
2022-10-24 14:08:48 +0400

    delete calculate

--- src/calculate/calculate.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CalculateController } from './calculate.controller';
-import { CalculateService } from './calculate.service';
-
-@Module({
-  controllers: [CalculateController],
-  providers: [CalculateService]
-})
-export class CalculateModule {}
