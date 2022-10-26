ec8d65534f6b3d9ffb7805403f875b0c21195ae8
Dmitrii <shokilla@mail.ru>
2022-10-26 13:58:59 +0400

    added dependenses + Multer

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
 import { CommentsController } from './controllers/comments.controller';
 import { NewsController } from './controllers/news.controller';
 import { CommentsModule } from './modules/comments/comments.module';
 import { NewsModule } from './modules/news/news.module';
 
 @Module({
-  imports: [NewsModule, CommentsModule],
+  imports: [
+    NewsModule,
+    CommentsModule,
+    MulterModule.register({
+      dest: './upload',
+    }),
+  ],
   controllers: [NewsController, CommentsController],
 })
 export class AppModule {}
